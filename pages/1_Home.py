ee223b4311033eb7452ca3e96a6baf84073016a2
Bano Rani <banor3106@gmail.com>
2026-07-16 18:10:09 +0500

    Update 1_Home.py

--- pages/1_Home.py
+++ pages/1_Home.py
@@ -1,1 +1,32 @@
+import streamlit as st
 
+
+st.title("🧬 About the Project")
+
+
+st.write(
+    """
+    The AI Biomarker Discovery Platform is a machine learning
+    based system designed to identify important biological
+    features from gene expression datasets.
+    """
+)
+
+
+st.subheader("Features")
+
+
+features = [
+    "Gene expression dataset analysis",
+    "Data preprocessing",
+    "Feature selection",
+    "Machine learning classification",
+    "Biomarker ranking",
+    "SHAP explainability",
+    "Biological interpretation",
+    "Automated reports"
+]
+
+
+for feature in features:
+    st.success(feature)
